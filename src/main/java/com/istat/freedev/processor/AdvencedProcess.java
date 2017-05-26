280559c561a254e638d0139f1f472823adb580af
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2017-05-26 00:04:59 +0000

    introduce AdvencedProcess

---
+++ src/main/java/com/istat/freedev/processor/AdvencedProcess.java
@@ -0,0 +1,18 @@
+package com.istat.freedev.processor;
+
+/**
+ * Created by istat on 25/05/17.
+ */
+
+public abstract class AdvencedProcess<Result, Error extends Throwable> extends Process<Result, Error> {
+
+    @Override
+    public boolean isRunning() {
+        return !isCanceled() && (getResult() == null || getError() == null);
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return !isCanceled() && (getResult() != null || getError() != null);
+    }
+}
